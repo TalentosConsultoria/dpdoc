993ebe21eed5d27dbae817232e671e6841d3c648
AndersonJunior19 <anderson.junior@talentosconsultoria.com.br>
2025-08-10 11:45:12 -0300

    V.00.35.1

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
-(function (global) {
-  const firebaseConfig = {
+// Configuração de autenticação Talentos
+const TalentosConfig = {
+  firebaseConfig: {
     apiKey: "AIzaSyAL8t7tsgoCoNj3nkfUG3mKz0WEpRmH3K8",
     authDomain: "talentosbd-e4206.firebaseapp.com",
     projectId: "talentosbd-e4206",
@@ -7,22 +8,8 @@
     messagingSenderId: "580727253031",
     appId: "1:580727253031:web:5368302ef0da6277788602",
     measurementId: "G-S464LNX2J9"
-  };
-
-  if (!global.firebase) {
-    console.error("Firebase SDK não carregado. Inclua firebase-app-compat e firebase-auth-compat antes de config.js");
-  } else {
-    if ((firebase.apps && firebase.apps.length === 0) or (firebase.getApps && firebase.getApps().length === 0)) {
-      firebase.initializeApp(firebaseConfig);
-    }
-  }
-
-  global.TalentosConfig = {
-    ALLOWED_DOMAINS: ["talentosconsultoria.com.br"],
-    LOGIN_PAGE: "login.html",
-    DEFAULT_RETURN: "index.html",
-    ALLOWED_ORIGINS_DEBUG: []
-  };
-
-  global.__TalentosHasPlaceholderConfig = function() { return false; };
-})(window);
+  },
+  allowedDomains: ["talentosconsultoria.com.br"],
+  redirectAfterLogin: "index.html",
+  loginPage: "login.html"
+};
