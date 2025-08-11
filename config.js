ba1570472f22381430c0ea957be4290ae734bad6
AndersonJunior19 <anderson.junior@talentosconsultoria.com.br>
2025-08-10 23:26:54 -0300

    V.00.38

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 (function (global) {
-  const firebaseConfig = {
-    apiKey: "AIzaSyAL8t7tsgoCoNj3nkfUG3mKz0WEpRmH3K8",
-    authDomain: "talentosbd-e4206.firebaseapp.com",
-    projectId: "talentosbd-e4206",
-    storageBucket: "talentosbd-e4206.firebasestorage.app",
-    messagingSenderId: "580727253031",
-    appId: "1:580727253031:web:5368302ef0da6277788602",
-    measurementId: "G-S464LNX2J9"
-  };
+ const firebaseConfig = {
+  apiKey: "AIzaSyAL8t7tsgoCoNj3nkfUG3mKz0WEpRmH3K8",
+  authDomain: "auth.talentosconsultoria.com.br",  // <<< AQUI
+  projectId: "talentosbd-e4206",
+  storageBucket: "talentosbd-e4206.firebasestorage.app",
+  messagingSenderId: "580727253031",
+  appId: "1:580727253031:web:5368302ef0da6277788602",
+  measurementId: "G-S464LNX2J9"
+};
 
   if (!global.firebase) { console.error("[config] Firebase SDK não carregado."); }
   else {
