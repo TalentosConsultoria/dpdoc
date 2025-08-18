ebd505203716234484cc701a6cca326e7c54504a
Yokoshima19 <anderson.junior@talentosconsultoria.com.br>
2025-08-18 17:11:03 -0300

    V.00.52.3

--- msal-config.js
+++ msal-config.js
@@ -9,8 +9,14 @@
   const CLIENT_ID = "aceb5b29-df99-4d90-8533-233407b08a2c";
   const TENANT_ID = "cba0be9f-94ad-4a26-b291-fa90af7491ee";
 
+<<<<<<< HEAD
   // Redirecionamentos
   const redirect = "https://dpdoc.talentosconsultoria.com.br/login.html";
+=======
+  // Observação: o redirectUri PRECISA existir nos "URIs de redirecionamento" do app no Azure.
+  // Deixe como index.html por padrão. Ajuste se hospedar em raiz (/).
+  const redirect = window.location.origin + "/index.html";
+>>>>>>> parent of 079de8f (V.00.51.7)
 
   window.MSAL_CONFIG = {
     auth: {
